b59705db04800f0bdbe5e140fe0fc804cb5e07f7
rishikeshmore5 <127827408+rishikeshmore5@users.noreply.github.com>
2026-08-04 14:46:42 +0530

    Update messageService.js

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -38,7 +38,7 @@ export async function sendMessage(message) {
 
     );
 
-console.log("Message Response:\n",JSON.stringify(response.data,null,2));
+console.log("Message Response:\n",JSON.stringify(response,null,2));
                 
     return response.data;
 }
